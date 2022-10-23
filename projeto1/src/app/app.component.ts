d760b597268786a60cb76c60c4a8f616ffb7feb0
Mateus Schwede <mateus1908.schwede@gmail.com>
2022-10-22 22:19:11 -0300

    Projeto1 (Compart de dados, input)

--- projeto1/src/app/app.component.ts
+++ projeto1/src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName: string = "UB Social";
+  userData = {
+    email: 'ubsocial@email.com',
+    role: 'Admin'
+  };
+
   title = 'projeto1';
 }
