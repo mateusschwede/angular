c58f0ab1b78e68f88d6c8dad19091245dd06426f
mateusschwede <mateus1908.schwede@gmail.com>
2022-11-07 17:54:49 -0300

    Projeto7 (Get e Search)

--- projeto7/moments/src/app/components/pages/home/home.component.ts
+++ projeto7/moments/src/app/components/pages/home/home.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { MomentService } from 'src/app/services/moment.service';
+import { Moment } from 'src/app/Moment';
+import { environment } from 'src/environments/environment';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
 
-  constructor() { }
+export class HomeComponent implements OnInit {
+  allMoments: Moment[] = [];
+  moments: Moment[] = [];
+  baseApiUrl = environment.apiBaseUrl;
+
+  faSearch = faSearch;
+  searchTerm: string = '';
+
+  constructor(private momentService: MomentService) {}
 
   ngOnInit(): void {
+    // Recepção de dados
+    this.momentService.getMoments().subscribe((items) => {
+      const data = items.data;
+      data.map((items) => {
+        items.created_at = new Date(items.created_at!).toLocaleString('pt-BR');
+      });
+      this.allMoments = data;
+      this.moments = data;
+    });
+  }
+
+  search(e: Event): void {
+    // Pesquisa de dados
+    const target = e.target as HTMLInputElement;
+    const value = target.value;
+    this.moments = this.allMoments.filter(moment => {
+      return moment.title.toLowerCase().includes(value);
+    });
   }
 
 }
--- projeto7/moments/src/app/services/moment.service.ts
+++ projeto7/moments/src/app/services/moment.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Moment } from '../Moment';
 import { environment } from 'src/environments/environment';
+import { Response } from '../Response';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +12,12 @@ export class MomentService {
   private baseApiUrl = environment.apiBaseUrl;
   private apiUrl = `${this.baseApiUrl}api/moments`;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
+
+  getMoments(): Observable<Response<Moment[]>> {
+    // Pegará dados da API Backend
+    return this.http.get<Response<Moment[]>>(this.apiUrl);
+  }
 
   createMoment(formData: FormData): Observable<FormData> {
     // Enviará dados para API Backend
