76d619fa2613b4d46dd5b2a8a0f9708a3660e1c8
mateusschwede <mateus1908.schwede@gmail.com>
2022-11-07 14:35:39 -0300

    Projeto7 (Add API Backend)

--- projeto7/moments/src/app/components/pages/new-moment/new-moment.component.ts
+++ projeto7/moments/src/app/components/pages/new-moment/new-moment.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Moment } from 'src/app/Moment';
+import { MomentService } from 'src/app/services/moment.service';
 
 @Component({
   selector: 'app-new-moment',
@@ -9,9 +11,26 @@ export class NewMomentComponent implements OnInit {
 
   btnText: string = "Adicionar";
 
-  constructor() { }
+  constructor(private momentService: MomentService) { }
 
   ngOnInit(): void {
+  }
+
+  async createHandler(moment: Moment) {
+    console.log('Enviado'); // Enviará form para service (Não será via JSON, será via FormData)
+
+    const formData = new FormData();
+    formData.append("title", moment.title);
+    formData.append("description", moment.description);
+    
+    if (moment.image) {
+      formData.append("image", moment.image);
+    }
+
+    await this.momentService.createMoment(formData).subscribe();
+
+    // Exibir msg (Feedback da operação)
+    // Redirect (Para a página home)
   }
 
 }
--- projeto7/moments/src/app/services/moment.service.ts
+++ projeto7/moments/src/app/services/moment.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Moment } from '../Moment';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MomentService {
+  private baseApiUrl = environment.apiBaseUrl;
+  private apiUrl = `${this.baseApiUrl}api/moments`;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createMoment(formData: FormData): Observable<FormData> {
+    // Enviará dados para API Backend
+    return this.http.post<FormData>(this.apiUrl, formData);
+  }
 }
